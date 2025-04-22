d10d97e4814453f093ba529f70641c24405525e5
Paul Mattern <pemattern@proton.me>
2025-04-22 09:21:50 +0200

    added user endpoints

--- src/routes/jwt.rs
+++ src/routes/jwt.rs
@@ -1,9 +1,12 @@
 use chrono::Utc;
 use std::time::Duration;
+use uuid::Uuid;
 
 use axum::{
     Extension, Json, Router,
+    extract::Request,
     http::StatusCode,
+    middleware::Next,
     response::{IntoResponse, Response},
     routing::get,
 };
@@ -11,7 +14,7 @@ use axum_extra::{
     TypedHeader,
     headers::{Authorization, authorization::Basic},
 };
-use jsonwebtoken::{EncodingKey, Header, encode};
+use jsonwebtoken::{DecodingKey, EncodingKey, Header, Validation, decode, encode};
 use serde::{Deserialize, Serialize};
 use sqlx::{PgPool, prelude::FromRow};
 
@@ -19,7 +22,7 @@ use sqlx::{PgPool, prelude::FromRow};
 pub struct Config {
     pub secret: String,
     pub issuer: String,
-    pub validity_duration: u64,
+    pub validity_secs: u64,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -54,7 +57,7 @@ pub async fn new(
     let config = Config {
         secret: "abc123".to_string(),
         issuer: "me".to_string(),
-        validity_duration: 3600,
+        validity_secs: 3600,
     };
     let sub = match sqlx::query_as::<_, GetUser>(
         "SELECT uuid FROM users WHERE name = $1 AND password = $2",
@@ -72,7 +75,7 @@ pub async fn new(
 
     let now = Utc::now();
     let iat = now.timestamp() as usize;
-    let expires_at = now + Duration::from_secs(config.validity_duration);
+    let expires_at = now + Duration::from_secs(config.validity_secs);
     let exp = expires_at.timestamp() as usize;
     let iss = config.issuer;
     let claims = Claims { sub, exp, iat, iss };
@@ -86,10 +89,52 @@ pub async fn new(
             Json(GetJWT {
                 access_token: jwt,
                 token_type: String::from("Bearer"),
-                expires_in: config.validity_duration,
+                expires_in: config.validity_secs,
             }),
         )
             .into_response(),
         Err(_) => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
     }
+}
+
+pub async fn authorize(
+    Extension(config): Extension<Config>,
+    mut request: Request,
+    next: Next,
+) -> Response {
+    let secret = config.secret;
+    let key = DecodingKey::from_secret(secret.as_bytes());
+    let issuer = config.issuer;
+    let mut validation = Validation::new(jsonwebtoken::Algorithm::HS256);
+    validation.set_issuer(&[issuer]);
+
+    let authorization_header = match request.headers().get("Authorization") {
+        Some(v) => v,
+        None => return StatusCode::UNAUTHORIZED.into_response(),
+    };
+
+    let authorization = match authorization_header.to_str() {
+        Ok(v) => v,
+        Err(_) => return StatusCode::UNAUTHORIZED.into_response(),
+    };
+
+    if !authorization.starts_with("Bearer ") {
+        return StatusCode::UNAUTHORIZED.into_response();
+    };
+
+    let jwt = authorization.trim_start_matches("Bearer ");
+
+    let claims =
+        match decode::<Claims>(&jwt, &key, &Validation::new(jsonwebtoken::Algorithm::HS256)) {
+            Ok(token_data) => token_data.claims,
+            Err(_) => return StatusCode::UNAUTHORIZED.into_response(),
+        };
+
+    let user = match Uuid::parse_str(&claims.sub) {
+        Ok(uuid) => uuid,
+        Err(_) => return (StatusCode::BAD_REQUEST, "invalid user").into_response(),
+    };
+
+    request.extensions_mut().insert(user);
+    next.run(request).await
 }
---
+++ src/routes/user.rs
@@ -0,0 +1,75 @@
+use crate::routes::jwt;
+use axum::{
+    Extension, Json, Router,
+    http::StatusCode,
+    middleware,
+    response::{IntoResponse, Response},
+    routing::get,
+};
+use chrono::{self, DateTime, Local};
+use serde::{Deserialize, Serialize};
+use sqlx::{FromRow, postgres::PgPool};
+use tower::ServiceBuilder;
+use uuid::Uuid;
+
+#[derive(Serialize, Deserialize, FromRow)]
+pub struct GetUser {
+    pub uuid: Uuid,
+    pub name: String,
+    pub password: String,
+    pub created_on: DateTime<Local>,
+    pub updated_on: DateTime<Local>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct PostUser {
+    pub name: String,
+    pub password: String,
+}
+
+pub fn router() -> Router {
+    Router::new()
+        .route("/user", get(get_user).post(post_user))
+        .layer(ServiceBuilder::new().layer(middleware::from_fn(jwt::authorize)))
+}
+
+#[axum::debug_handler]
+pub async fn get_user(
+    Extension(pool): Extension<PgPool>,
+    Extension(user): Extension<Uuid>,
+) -> Response {
+    match sqlx::query_as::<_, GetUser>("SELECT * FROM users WHERE uuid = $1")
+        .bind(&user)
+        .fetch_one(&pool)
+        .await
+    {
+        Ok(user) => (StatusCode::OK, Json(user)).into_response(),
+        Err(sqlx::Error::RowNotFound) => StatusCode::NOT_FOUND.into_response(),
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+    }
+}
+
+pub async fn post_user(Extension(pool): Extension<PgPool>, Json(user): Json<PostUser>) -> Response {
+    let now = chrono::offset::Local::now();
+
+    match sqlx::query("INSERT INTO users (uuid, name, password, created_on, updated_on) VALUES ($1, $2, $3, $4, $5)")
+        .bind(Uuid::now_v7())
+        .bind(&user.name)
+        .bind(&user.password)
+        .bind(now)
+        .bind(now)
+        .execute(&pool)
+        .await
+    {
+        Ok(_) => StatusCode::CREATED.into_response(),
+        Err(sqlx::Error::Database(error)) => {
+            // Unique violation code
+            if error.code().unwrap() == "23505" {
+                StatusCode::CONFLICT.into_response()
+            } else {
+                StatusCode::BAD_REQUEST.into_response()
+            }
+        }
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+    }
+}
