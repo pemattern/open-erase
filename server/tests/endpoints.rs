fdda90df38aefe1f6d095eaf1f10fbb0c6678780
Paul Mattern <pemattern@proton.me>
2025-10-23 15:07:37 +0200

    deleted folder

--- server/tests/endpoints.rs
+++
@@ -1,5 +0,0 @@
-use axum::{body::Body, extract::Request, http::StatusCode};
-use base64::{Engine, prelude::BASE64_STANDARD};
-use server::{models::User, schemas::user::PostUserRequest, state::AppState};
-use tower::ServiceExt;
-use uuid::Uuid;
